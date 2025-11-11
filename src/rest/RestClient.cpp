16d80b3afd2b8d4e47d34f8f36b0309496b76bf9
Robert Yokota <rayokota@gmail.com>
2025-11-10 17:21:31 -0800

    Add new header (#11)

--- src/rest/RestClient.cpp
+++ src/rest/RestClient.cpp
@@ -135,7 +135,7 @@ cpr::Response RestClient::sendRequest(
     // Set default headers including content type
     cpr::Header cpr_headers;
     cpr_headers["Content-Type"] = "application/vnd.schemaregistry.v1+json";
-    cpr_headers["Accept-Version"] = "8.0";
+    cpr_headers["Confluent-Accept-Unknown-Properties"] = "true";
 
     // Handle authentication
     const auto basic_auth = configuration_->getBasicAuth();
